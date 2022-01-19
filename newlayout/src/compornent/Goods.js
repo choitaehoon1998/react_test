de8845d1e3ca61f72211d40eb544faacf3d10d4d
mmminJJeong <flzn0100@gmail.com>
2022-01-19 18:16:48 +0900

    .

---
+++ newlayout/src/compornent/Goods.js
@@ -0,0 +1,15 @@
+export default function Goods(){
+    return(
+        <>
+        <div className="content-wrap">
+            <div className="content">
+                <div className="content-main">
+                    <div className="content-title">
+                        <h3>상품</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
---
+++ newlayout/src/compornent/Order.js
@@ -0,0 +1,15 @@
+export default function Order(){
+    return(
+        <>
+        <div className="content-wrap">
+            <div className="content">
+                <div className="content-main">
+                    <div className="content-title">
+                        <h3>주문/배송</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
