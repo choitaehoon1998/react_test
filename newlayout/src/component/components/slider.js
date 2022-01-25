67a4acf1ef8f176996aeadb480b524f23ce91dda
wjsalsgus1 <97424180+wjsalsgus1@users.noreply.github.com>
2022-01-25 09:18:00 +0900

    .

--- newlayout/src/component/components/slider.js
+++ newlayout/src/component/components/slider.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 export const Slider = (props) => {
+    
     const gapSlider = document.getElementById("myRange");
-    const y= document.getElementById("f");
-    y.innerHTML = gapSlider.value;
-    gapSlider.oninput = function(){
-        y.innerHTML=this.value;
-    }
+    debugger
+        const y= document.getElementById("f");
+        y.innerHTML = gapSlider.value;
+        gapSlider.oninput = function(){
+        y.innerHTML=this.value
+        }
     
     return(
+        <>
         <div className="oneslide">
-            <input type="range" id="myRange" name="slider" min={props.min} max={props.max}/>
-            <span id="f">100</span>
+            <input type="range" id="myRange" name="slider" min={props.min} max={props.max} />
+            <span id="f"></span>
         </div>
+        
+        
+        </>
     );
     
 }
