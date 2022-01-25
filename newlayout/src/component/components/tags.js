16d0c36415ed7f16cc00085fc6a5ee61320d152f
wjsalsgus1 <97424180+wjsalsgus1@users.noreply.github.com>
2022-01-25 18:29:49 +0900

    .

--- newlayout/src/component/components/tags.js
+++ newlayout/src/component/components/tags.js
@@ -1,73 +1,46 @@
-import React,{useEffect} from "react";
-import "./compostyle.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import TagsInput from "react-tagsinput";
+import "react-tagsinput/react-tagsinput.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const Tags = () => {
-    document.addEventListener("DOMContentLoaded",function () {
-        var tag = {};
-        var counter = 0;
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { tags: [] };
+  }
 
-        function addTag (value){
-            tag[counter] = value;
-            counter++;
-        }
+  handleChange = (tags) => {
+    console.log(tags);
+    this.setState({ tags });
+  };
 
-        function marginTag () {
-            return Object.values(tag).filter(function (word){
-                return word !== "";
-            });
-        }
-
-        ("#tag-form").on("submit", function (e){
-            let value = marginTag();
-            ("#rdTag").val(value);
-
-            (this).submit();
-            
-        });
-
-        ("#tag").on("keypress",function (e){
-            var self = (this);
-
-            if(e.key === "Enter" || e.keyCode == 32){
-
-                var tagValue = self.val();
-                console.log(tagValue);
-                debugger
-                if(tagValue !== ""){
-
-                    var result = Object.values(tag).filter(function (word){
-                        return word === tagValue;
-                    })
-                    if(result.length == 0 ){
-                        ("#tag-list").append("<li class='tag-item'>"+tagValue+"<span class='del-btn' idx='"+counter+"'>x</span></li>");
-                        addTag(tagValue);
-                        self.val("");
-                    }else{
-                    }
-                }
-                e.preventDefault();
-                
-            }
-        });
-        (document).on("click", ".del-btn", function(e){
-            var index = (this).attr("idx");
-            tag[index]="";
-            (this).parent().remove();
-        });
-        
-    })
-    return(
-        <>
-        <div>
-            <input type="hidden" value="" name="tag" id="rdTag" />
-        </div>
-        
-        <ul id="tag-list"></ul>
-
-        <div>
-            <input type="text" id="tag" />
-        </div>
-        </>
+  render() {
+    console.log(this.state);
+    return (
+      <Formik
+        initialValues={this.state}
+        onSubmit={this.onSubmit}
+        render={({
+          values,
+          handleSubmit,
+          setFieldValue,
+        }) => (
+          <Form onSubmit={handleSubmit} noValidate name="simpleForm">
+            <TagsInput
+              name="tags"
+              value={values.tags}
+              onChange={(tags) => {
+                console.log(tags);
+                setFieldValue("tags", tags);
+              }}
+            />
+          </Form>
+        )}
+      />
     );
+  }
 }
-export default Tags;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
