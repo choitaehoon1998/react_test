ed1f35b05c00efdac02a8ab0fe2e73c2bb80be57
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-01-25 10:01:47 +0900

    modal
    modal

--- newlayout/src/component/components/slider.js
+++ newlayout/src/component/components/slider.js
@@ -3,12 +3,12 @@ import React from "react";
 export const Slider = (props) => {
     
     const gapSlider = document.getElementById("myRange");
-    debugger
-        const y= document.getElementById("f");
-        y.innerHTML = gapSlider.value;
-        gapSlider.oninput = function(){
-        y.innerHTML=this.value
-        }
+
+        // const y= document.getElementById("f");
+        // y.innerHTML = gapSlider.value;
+        // gapSlider.oninput = function(){
+        // y.innerHTML=this.value
+        // }
     
     return(
         <>
---
+++ newlayout/src/component/modal/ChildModal.js
@@ -0,0 +1,48 @@
+import ReactModal from "react-modal";
+
+
+
+const ChildModal = ({openModal,onChildSubmit,onChildCancel}) => {
+    
+    const handleClickSubmit = () => {
+        onChildSubmit();
+    }
+
+    const handleClickCancel = () => {
+        onChildCancel();
+    }
+
+    const testStyle = 14;
+    const translate = "translate(-50%,-50%)";
+
+    const modalStyle = {
+        overlay : {
+            position: "fixed",
+            zIndex: 14,
+        },
+
+        content: {
+            position: "fixed",
+            width: "700px",
+            height : "500px",
+            top: "50%",
+            left: "50%", 
+            transform : translate,
+            zIndex: testStyle,
+        }
+    }
+
+    return(
+        <>
+        <ReactModal isOpen={openModal} style={modalStyle}>
+        <button id="save" onClick={handleClickSubmit}>저장</button>
+            <button id="modify" onClick={handleClickSubmit}>수정</button>
+            <button id="remove" onClick={handleClickSubmit}>삭제</button>
+            <button id="cancel" onClick={handleClickCancel}>취소</button>
+            <div>하위 모달입니다</div>
+        </ReactModal>
+        </>
+    );
+}
+
+export default ChildModal;
