a52617f2574247017f54323895521d5efef1d802
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-01-26 11:04:29 +0900

    autosuggest
    autosuggest

--- newlayout/src/ComponentPage/components.js
+++ newlayout/src/ComponentPage/components.js
@@ -7,6 +7,7 @@ import CheckboxCoponent from "../component/components/checkbox";
 import { CountingStar } from "../component/components/countingstar";
 import { Tags } from "../component/components/tags";
 import {DropZone} from "../component/components/dropzone";
+import AutosuggestComponent from "../component/components/autosuggestComponent";
 
 export default function Components() {
     return( 
@@ -32,6 +33,20 @@ export default function Components() {
                             </label>
                             <div className="form-groups">
                                 
+                            </div>
+                        
+
+                        </div>
+                    </div>
+
+
+                    <div className="left-frame">
+                        <div className="form-frame frame1">
+                            <label>
+                                Custom Autosuggest
+                            </label>
+                            <div className="form-groups">
+                                <AutosuggestComponent></AutosuggestComponent>
                             </div>
                         
 
